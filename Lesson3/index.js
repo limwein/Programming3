ea83bf14343853bbfa49a31dd83dce5347cee867
limwein <vardan.a1.y@tumo.org>
2019-06-09 00:30:34 +0400

    json

--- Lesson3/index.js
+++ Lesson3/index.js
@@ -1,8 +1,31 @@
-let os = require("os");
-let message = "The platform is ";
+let fs = require("fs");
 
-function main() {
-    console.log(message + os.platform());
+function test() {
+    fs.writeFileSync("hello.txt", "hello world \n", function (err) {
+        console.log("fswriteFile ended");
+    });
+    console.log("writefile");
 }
 
-main();
+let dummytext = "Apple yep";
+
+function main() {
+    fs.writeFileSync("dummytext.txt", dummytext);
+    let text = fs.readFileSync("dummytext.txt").toString();
+    console.log(dummytext == text);
+    console.log(text);
+    fs.writeFileSync("undummytext.txt",
+        text.replace("Apple", "Microsoft"));
+}
+
+let obj = {
+    "first_name" : "Vardan",
+    "last_name" : "Arevshatyan",
+    "age" : 19,
+    "tumo_student" : true
+}
+
+function create() {
+    fs.writeFileSync("obj.json", JSON.stringify(obj));
+}
+create();
