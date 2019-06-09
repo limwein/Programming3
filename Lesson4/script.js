e1f706f5bc6572d0a49bf71f601001e9bde55b57
limwein <vardan.a1.y@tumo.org>
2019-06-09 17:37:39 +0400

    socket

--- Lesson4/script.js
+++ Lesson4/script.js
@@ -1,10 +1,27 @@
-var clickCount = 0;
-function clickHandler(evt) {
-    clickCount++;
-    console.log(evt);
-    var str = "Thanks for clicking " + clickCount;
-    this.innerText = str;
+function main() {
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
+        }
+    }
 }
 
-var p = document.getElementById("pElement");
-p.addEventListener("click", clickHandler);
+button.onclick = handleSubmit;
+
+function handleMessage(msg) {
+    var p = document.createElement('p');
+    p.innerText = msg;
+    chatDiv.appendChild(p);
+    input.value = "";
+}
+
+socket.on('display message', handleMessage);
+// main closing bracket
+
+window.onload = main;   
